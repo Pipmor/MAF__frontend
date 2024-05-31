30205c3c7564e30c1a9bb8918dc21529f7068213
Pipmor <139385966+Pipmor@users.noreply.github.com>
2024-05-31 11:15:56 +0600

    newRouter

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Router from './pages/Router/Router';
-import LanguageSelector from "./components/LanguageSwitcher/LanguageSwitcher.jsx";
-
+import { RouterProvider } from 'react-router-dom'
+import { router } from './pages/Router/Router.jsx'
 import { I18nextProvider } from 'react-i18next';
 import i18n from 'i18next';
 import ruTranslations from '../src/localosation/ru.json';
 import kgTranslations from '../src/localosation/kg.json';
+
 
 
 i18n.init({
@@ -28,7 +28,7 @@ i18n.init({
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
         <I18nextProvider i18n={i18n}>
-            <Router />
+            <RouterProvider router={router} />
         </I18nextProvider>
     </React.StrictMode>,
     document.getElementById('root')
--- src/pages/Router/Router.jsx
+++ src/pages/Router/Router.jsx
@@ -1,4 +1,4 @@
-import { createBrowserRouter, NavLink, RouterProvider } from "react-router-dom";
+import { createBrowserRouter } from "react-router-dom";
 import Home from "../Home/Home.jsx";
 import Products from "../Products/Products.jsx";
 import Contacts from "../Contacts/Contacts.jsx";
@@ -6,9 +6,7 @@ import About from "../About/About.jsx";
 import Press from "../Press/Press.jsx";
 import Questions from "../Questions/Questions.jsx";
 import Layout from "../../components/Layout/Layout.jsx";
-import styles from "../../components/Breadcrumbs/Breadcrumbs.module.css";
 import ErrorPage from "../ErrorPage/ErrorPage.jsx";
-import {getProductById} from "../../api/getProductById.js";
 import ProductDetail from "../DetailPage/ProductDetail.jsx";
 import CalendarPage from "../../components/Сalendar/Calendar.jsx";
 import NewsPage from "../../components/Publish/Publish.jsx";
@@ -18,184 +16,74 @@ import Vaccine from "../../components/Vaccine/Vaccine.jsx";
 import ProductNew from "../../components/ProductNew/ProductNew.jsx";
 import ServicePage from "../ServicePage/ServicePage.jsx";
 
-
-const Router = () => {
-  const router = createBrowserRouter([
-    {
-      path: "/",
-      element: <Layout />,
-      errorElement: <ErrorPage />,
-
-      handle: {
-        crumb: () => (
-          <NavLink to="/" className={styles.navLink}>
-            Главная
-          </NavLink>
-        ),
+export const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Layout />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: '/',
+        element: <Home />,
       },
-      children: [
-        { index: true, element: <Home /> },
-
-        {
-          path: "services",
-          element: <ServicePage />,
-          handle: {
-            crumb: () => (
-                <NavLink to="/services" className={styles.navLink} end>
-                  Услуги
-                </NavLink>
-            ),
+      {
+        path: 'services',
+        element: <ServicePage />,
+      },
+      {
+        path: 'products',
+        element: <Products />,
+        children: [
+          {
+            path: '/products/:productId',
+            element: <ProductDetail />,
           },
-        },
-        {
-          path: "products",
-          element: <Products />,
-          handle: {
-            crumb: () => (
-              <NavLink to="/products" className={styles.navLink} end>
-                Продукция
-              </NavLink>
-            ),
-          },
-          children: [
-            {
-              path: ':productId',
-              element: <ProductDetail />,
-              loader: (params) => getProductById({ params: { id: parseInt(params.productId, 10) } }),
-              handle: {
-                crumb: (data) => <span className={styles.span}>{data.title}</span>,
-              },
-            },
-          ],
-        },
-        {
-          path: "vaccine",
-          element: <Vaccine/>,
-          handle: {
-            crumb: () => (
-                <NavLink to="/vaccine" className={styles.navLink} end>
-                  Вакцины
-                </NavLink>
-            ),
-          },
-        },
-        {
-          path: "ProductNew",
-          element: <ProductNew/>,
-          handle: {
-            crumb: () => (
-                <NavLink to="/productNew" className={styles.navLink} end>
-                  Новинки
-                </NavLink>
-            ),
-          },
-        },
-        {
-          path: "ProductDetail",
-          element: <ProductDetail/>,
-          handle: {
-            crumb: () => (
-                <NavLink to="/productDetail" className={styles.navLink} end>
-                  ТЩАЗ
-                </NavLink>
-            ),
-          },
-        },
-
-        {
-          path: "press",
-          element: <Press />,
-          handle: {
-            crumb: () => (
-                <NavLink to="/press" className={styles.navLink} end>
-                  Пресс центр
-                </NavLink>
-            ),
-          },
-        },
-        {
-          path: "events", 
-          element: <EventsPage />, 
-          handle: {
-            crumb: () => (
-                <NavLink to="/events" className={styles.navLink} end>
-                  События
-                </NavLink>
-            ),
-          },
-        },
-
-        {
-          path: "news",
-          element: <NewsPage />,
-          handle: {
-            crumb: () => (
-                <NavLink to="/news" className={styles.navLink} end>
-                  Новости
-                </NavLink>
-            ),
-          },
-        },
-        {
-          path: "calendar",
-          element: <CalendarPage />,
-          handle: {
-            crumb: () => (
-                <NavLink to="/calendar" className={styles.navLink} end>
-                  Календарь
-                </NavLink>
-            ),
-          },
-        },
-        {
-          path: "newsvideo",
-          element: <NewsVideo />,
-          handle: {
-            crumb: () => (
-                <NavLink to="/newsvideo" className={styles.navLink} end>
-                  Видео
-                </NavLink>
-            ),
-          },
-        },
-        {
-          path: "questions",
-          element: <Questions />,
-          handle: {
-            crumb: () => (
-                <NavLink to="/questions" className={styles.navLink} end>
-                  Вопросы и Ответы
-                </NavLink>
-            ),
-          },
-        },
-        {
-          path: "about",
-          element: <About />,
-          handle: {
-            crumb: () => (
-              <NavLink to="/about" className={styles.navLink} end>
-                О нас
-              </NavLink>
-            ),
-          },
-        },
-        {
-          path: "contacts",
-          element: <Contacts />,
-          handle: {
-            crumb: () => (
-              <NavLink to="/contacts" className={styles.navLink} end>
-                Контакты
-              </NavLink>
-            ),
-          },
-        },
-      ],
-    },
-  ]);
-
-  return <RouterProvider router={router} />;
-};
-
-export default Router;
+        ],
+      },
+      {
+        path: 'vaccine',
+        element: <Vaccine />,
+      },
+      {
+        path: 'productNew',
+        element: <ProductNew />,
+      },
+      {
+        path: 'productDetail',
+        element: <ProductDetail />,
+      },
+      {
+        path: 'press',
+        element: <Press />,
+      },
+      {
+        path: 'events',
+        element: <EventsPage />,
+      },
+      {
+        path: 'news',
+        element: <NewsPage />,
+      },
+      {
+        path: 'calendar',
+        element: <CalendarPage />,
+      },
+      {
+        path: 'newsvideo',
+        element: <NewsVideo />,
+      },
+      {
+        path: 'questions',
+        element: <Questions />,
+      },
+      {
+        path: 'about',
+        element: <About />,
+      },
+      {
+        path: 'contacts',
+        element: <Contacts />,
+      },
+    ],
+  },
+]);

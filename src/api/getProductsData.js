5331438e7120d89343d3c13d155849c156054a56
Pipmor <139385966+Pipmor@users.noreply.github.com>
2024-06-19 15:44:49 +0600

    production

--- src/api/getProductsData.js
+++ src/api/getProductsData.js
@@ -11,6 +11,20 @@ export const getProductsData = async () => {
     return response.data;
   } catch (error) {
     console.error("Failed to fetch products data:", error);
+
+    if (error.response) {
+      // Сервер ответил с кодом статуса, который выходит за пределы диапазона 2xx
+      console.error('Response error data:', error.response.data);
+      console.error('Response error status:', error.response.status);
+      console.error('Response error headers:', error.response.headers);
+    } else if (error.request) {
+      // Запрос был сделан, но ответа не получено
+      console.error('Request error:', error.request);
+    } else {
+      // Ошибка при настройке запроса
+      console.error('Error message:', error.message);
+    }
+
     throw error;
   }
 };
